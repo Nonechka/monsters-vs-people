8a4f26a94f548e6446d0263eb69c3c0a44055859
Nonechka <arudoj116@gmail.com>
2024-01-13 11:31:43 +0200

    home 13.01.24

--- Assets/Scripts/Monsters.cs
+++ Assets/Scripts/Monsters.cs
@@ -7,7 +7,7 @@ public class Monsters : MonoBehaviour
     public float MoveSpeed;
 
     private Transform _transform;
-    private float _deathZone = -7.68f;
+    private float _deathZone = -4.72f;
 
 
 
--- Assets/Scripts/MonstersSpawner.cs
+++ Assets/Scripts/MonstersSpawner.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class MonstersSpawner : MonoBehaviour
 {
     private float _borderPositionY = 6.18f;
-    private float _borderPositionXrigth = 5.26f;
-    private float _borderPositionXleft = -10.89f;
+    private float _borderPositionXrigth = 3.54f;
+    private float _borderPositionXleft = -7f;
     private int _enemySpawnTime = 2;
 
+    private float _upBorderPositionY = 4f;
+    private float _downBorderPositionY = -2f;
+    private float _borderPositionX = -10f;
+
+
+
     public GameObject MonstersPrefab;
+    public GameObject MonstersPrefab2;
 
     private void Start()
     {
@@ -20,6 +27,7 @@ public class MonstersSpawner : MonoBehaviour
         while (true)
         {
             MonstersSpawn();
+            MonstersSpawn2();
             yield return new WaitForSeconds(_enemySpawnTime);
         }
        
@@ -28,5 +36,9 @@ public class MonstersSpawner : MonoBehaviour
     {
         Instantiate(MonstersPrefab, new Vector3(Random.Range(_borderPositionXleft, _borderPositionXrigth), _borderPositionY, transform.position.z), Quaternion.identity);
 
+    }
+    private void MonstersSpawn2()
+    {
+        Instantiate(MonstersPrefab2, new Vector3(_borderPositionX, Random.Range(_downBorderPositionY, _upBorderPositionY), transform.position.z), Quaternion.Euler(0, 180, 0));
     }
 }
