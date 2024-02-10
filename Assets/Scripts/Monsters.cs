03eedbd474a4d15a9f150b060855122f792ed740
Nonechka <arudoj116@gmail.com>
2024-02-10 11:32:46 +0200

    home 10.02.24

--- Assets/Scripts/Monsters.cs
+++ Assets/Scripts/Monsters.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class Monsters : MonoBehaviour
 {
+    public int MonstersLives;
     public float MoveSpeed;
 
     private Transform _transform;
     private float _deathZone = -4.72f;
-
-
 
     private void Awake()
     {
@@ -22,8 +21,19 @@ public class Monsters : MonoBehaviour
         if (_transform.position.y < _deathZone)
         {
             Destroy(gameObject);
-        
+        }
+
+        if(MonstersLives == 0)
+        {
+            Destroy(gameObject);
         }
     }
-    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("laser"))
+        {
+            MonstersLives--;
+        }
+    }
+
 }
